507b364ece7710fca0052b0d579ec2ba32bd071a
wunlei <flowersonviolent@gmail.com>
2020-10-03 17:44:41 +0300

    feat: add algorithm codes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    function compare(a, b) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      if (a === b) return 0;
+    };
+    let sortArray = array.sort(compare);
+    return sortArray[0];
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    function compare(a, b) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      if (a === b) return 0;
+    };
+    let sortArray = array.sort(compare);
+    return sortArray[array.length - 1];
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (Array.isArray(array) && array.length > 0) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    return sum / array.length;
+  }
   return 0;
 }
